e85d9add14ecc43e8260481be1e71d1f07c6d87e
GeorgeErickson <george55@mit.edu>
2014-03-26 22:41:47 -0400

    Compiling

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,19 @@
-var __hasProp = {}.hasOwnProperty,
-  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
-
-angular.module('app', ['ui.router', 'ngSanitize']).service('Algolia', function() {
+angular.module('app', ['ui.router', 'ngSanitize', 'ui.bootstrap']).service('Algolia', function() {
   return new AlgoliaSearch('OMJJSUW8EV', '677bfc2f1458fa602cb88c825c4d531b');
-}).config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+}).config(function($stateProvider, $urlRouterProvider, $locationProvider, $tooltipProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise("/");
   $stateProvider.state('search', {
     url: '/',
-    templateUrl: 'templates/search.html',
+    templateUrl: '/templates/search.html',
     controller: 'SearchCtrl'
+  }).state('detail', {
+    url: '/library/:name',
+    templateUrl: '/templates/detail.html',
+    controller: 'DetailCtrl'
+  });
+  $tooltipProvider.setTriggers({
+    'open': 'close'
   });
   return ZeroClipboard.config({
     moviePath: '//cdn.jsdelivr.net/zeroclipboard/1.3.3/ZeroClipboard.swf',
@@ -55,9 +59,25 @@ angular.module('app', ['ui.router', 'ngSanitize']).service('Algolia', function()
   return {
     restrict: 'A',
     link: function(scope, element, attrs) {
-      var clip;
+      var clip, text;
+      text = 'copy to clipboard';
+      scope.tooltip = text;
       clip = new ZeroClipboard(element);
       return clip.on("load", function(client) {
+        client.on('mouseover', function() {
+          return element.triggerHandler('mouseenter');
+        });
+        client.on('mouseout', function() {
+          element.triggerHandler('mouseleave');
+          return scope.tooltip = text;
+        });
+        client.on('complete', function() {
+          scope.$apply(function() {
+            return scope.tooltip = 'copied!';
+          });
+          element.triggerHandler('mouseleave');
+          return element.triggerHandler('mouseenter');
+        });
         return scope.$on("$destroy", function() {
           return client.unclip(element);
         });
@@ -72,6 +92,7 @@ angular.module('app', ['ui.router', 'ngSanitize']).service('Algolia', function()
       console.log(_.first(scope.hits));
       scope.info = this.info(response);
       scope.page = this.page(response);
+      console.log(scope.page);
     }
 
     SearchResponse.prototype.info = function(data) {
@@ -106,52 +127,32 @@ angular.module('app', ['ui.router', 'ngSanitize']).service('Algolia', function()
 
   })();
 }).service('Library', function(Index, $location, SearchResponse) {
-  var Library, LibrarySearchResponse, index, _ref;
+  var Library, index;
   index = new Index('jsdelivr');
-  LibrarySearchResponse = (function(_super) {
-    __extends(LibrarySearchResponse, _super);
-
-    function LibrarySearchResponse() {
-      _ref = LibrarySearchResponse.__super__.constructor.apply(this, arguments);
-      return _ref;
-    }
-
-    LibrarySearchResponse.prototype.hits = function(data) {
-      return _.map(LibrarySearchResponse.__super__.hits.call(this, data), function(lib) {
-        var assets;
-        assets = _.indexBy(lib.assets, 'version');
-        _.each(assets, function(a) {
-          return a.files = _.groupBy(a.files, function(fn) {
-            return fn.split('.')[0];
-          });
-        });
-        lib.assets = assets;
-        return lib;
-      });
-    };
-
-    return LibrarySearchResponse;
-
-  })(SearchResponse);
   Library = (function() {
     function Library() {}
+
+    Library.prototype.get = function(id, params) {
+      return index.getObject(id, params);
+    };
 
     Library.prototype.search = function(scope) {
       var params, state;
       state = $location.search();
       params = {
-        hitsPerPage: 10,
+        hitsPerPage: 25,
         facets: ['*'],
         maxValuesPerFacet: 10,
         facetFilters: [],
+        attributesToRetrieve: ['description', 'name', 'lastversion'],
         q: state.q
       };
       if (state.page) {
         params.page = state.page;
       }
-      scope.hits = [];
+      scope.hits = null;
       return index.search(params).then(function(response) {
-        return new LibrarySearchResponse(scope, response);
+        return new SearchResponse(scope, response);
       });
     };
 
@@ -159,9 +160,23 @@ angular.module('app', ['ui.router', 'ngSanitize']).service('Algolia', function()
 
   })();
   return new Library();
+}).controller('DetailCtrl', function($scope, $stateParams, Library) {
+  $scope.copy_as = 'url';
+  Library.get($stateParams.name).then(function(res) {
+    res.selected_version = res.lastversion;
+    res.assets = _.indexBy(res.assets, 'version');
+    return $scope.lib = res;
+  });
+  return $scope.file_url = function() {
+    return "//cdn.jsdelivr.net/" + this.lib.selected_version + "/" + this.file;
+  };
 }).controller('SearchCtrl', function($scope, $location, Library) {
+  $scope.search = $location.search().q;
   Library.search($scope);
-  $scope.assets_for_version = function(version) {};
+  $scope.reset = function() {
+    $location.search({});
+    return Library.search($scope);
+  };
   $scope.select = function(param_key, value) {
     $location.search('page', null);
     if (value === "") {
@@ -171,6 +186,9 @@ angular.module('app', ['ui.router', 'ngSanitize']).service('Algolia', function()
     return Library.search($scope);
   };
   return $scope.select_page = function(page) {
+    if (page < 0) {
+      return;
+    }
     if (page === 0) {
       page = null;
     }
