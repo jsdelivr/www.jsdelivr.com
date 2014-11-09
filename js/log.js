7a4e183d9eb5c48cfcd910f1873d3bbe05ee42ab
Martin Kolárik <martin@kolarik.sk>
2014-11-09 21:26:28 +0100

    Use console.log() for logging during development

--- js/log.js
+++ js/log.js
@@ -8,5 +8,11 @@ module.exports = function (name) {
 		console.error('Log error: %s', error);
 	});
 
+	if (process.env.NODE_ENV === 'development') {
+		log.on('log', function (message) {
+			console.log(message);
+		});
+	}
+
 	return log;
 };
