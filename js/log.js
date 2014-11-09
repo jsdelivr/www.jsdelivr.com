e597927fba684b2a7484b906866409e0778f335f
Martin Kolárik <martin@kolarik.sk>
2014-11-09 21:58:12 +0100

    Remove unnecessary error listener

--- js/log.js
+++ js/log.js
@@ -4,10 +4,6 @@ var config = require('./config/logentries.js');
 module.exports = function (name) {
 	var log = logentries.logger(config[name]);
 
-	log.on('error', function (error) {
-		console.error('Log error: %s', error);
-	});
-
 	if (process.env.NODE_ENV === 'development') {
 		log.on('log', function (message) {
 			console.log(message);
