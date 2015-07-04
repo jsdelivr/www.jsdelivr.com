714b400c1cea342927a3c7a0b8f77506f4710d8b
Martin Kolárik <martin@kolarik.sk>
2015-07-04 14:42:11 +0200

    Add tooltips to zeroclipboard buttons

--- src/public/js/decorators/zero-clipboard.js
+++ src/public/js/decorators/zero-clipboard.js
@@ -1,34 +1,28 @@
-export default function (node/*, tooltipPlacement = 'right'*/) {
-	/*let clip = */new ZeroClipboard(node);
-	/*let $node = $(node);
+export default function (node, tooltipPlacement = 'top') {
+	let clip = new ZeroClipboard(node);
+	let $node = $(node);
+	let tooltipOptions = {
+		title: 'Copy to Clipboard',
+		placement: tooltipPlacement,
+		trigger: 'hover',
+		container: 'body',
+		animation: false,
+	};
 
 	$node
 		.on('mouseover', () => {
 			$node
 				.tooltip('destroy')
-				.tooltip({
-					title: 'Copy to Clipboard',
-					placement: tooltipPlacement,
-					trigger: 'manual',
-					container: 'body',
-				})
+				.tooltip(tooltipOptions)
 				.tooltip('show');
-		})
-		.on('mouseout', () => {
-			$node.tooltip('destroy');
 		});
 
 	clip.on('aftercopy', () => {
 		$node
 			.tooltip('destroy')
-			.tooltip({
-				title: 'Copied!',
-				placement: tooltipPlacement,
-				trigger: 'manual',
-				container: 'body',
-			})
+			.tooltip($.extend({}, tooltipOptions, { title: 'Copied!' }))
 			.tooltip('show');
-	});*/
+	});
 
 	return {
 		teardown: () => {}
