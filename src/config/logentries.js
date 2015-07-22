8567b980aac9a80e82e596078fe27d92379e2364
Martin Kolárik <martin@kolarik.sk>
2015-07-22 10:13:09 +0200

    Deps: use new logentries module

--- src/config/logentries.js
+++ src/config/logentries.js
@@ -1,8 +1,9 @@
 export default {
+	// Using fake tokens when ENV vars aren't set so that it doesn't throw and we get logging to the console instead.
 	access: {
-		token: process.env.LOGENTRIES_ACCESS_TOKEN
+		token: process.env.LOGENTRIES_ACCESS_TOKEN || 'aaaaaaaa-1111-1111-1111-aaaaaaaaaaaa',
 	},
 	app: {
-		token: process.env.LOGENTRIES_APP_TOKEN
+		token: process.env.LOGENTRIES_APP_TOKEN || 'aaaaaaaa-1111-1111-1111-aaaaaaaaaaaa',
 	},
 };
--- src/js/logger.js
+++ src/js/logger.js
@@ -1,8 +1,8 @@
-import logentries from 'node-logentries';
+import Logger from 'le_node';
 import config from '../config/logentries';
 
 export default function (name) {
-	let log = logentries.logger(config[name]);
+	let log = new Logger(config[name]);
 
 	if (process.env.NODE_ENV === 'development') {
 		log.on('log', (message) => {
