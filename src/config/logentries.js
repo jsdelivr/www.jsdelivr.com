4b846fa742552816325f103a214744a4e2e72667
Martin Kolárik <martin@kolarik.sk>
2015-07-22 12:16:08 +0200

    Update logentries config

--- src/config/logentries.js
+++ src/config/logentries.js
@@ -2,8 +2,14 @@ export default {
 	// Using fake tokens when ENV vars aren't set so that it doesn't throw and we get logging to the console instead.
 	access: {
 		token: process.env.LOGENTRIES_ACCESS_TOKEN || 'aaaaaaaa-1111-1111-1111-aaaaaaaaaaaa',
+		console: process.env.NODE_ENV === 'development',
+		timestamp: true,
+		withStack: true,
 	},
 	app: {
 		token: process.env.LOGENTRIES_APP_TOKEN || 'aaaaaaaa-1111-1111-1111-aaaaaaaaaaaa',
+		console: process.env.NODE_ENV === 'development',
+		timestamp: true,
+		withStack: true,
 	},
 };
--- src/js/logger.js
+++ src/js/logger.js
@@ -2,13 +2,5 @@ import Logger from 'le_node';
 import config from '../config/logentries';
 
 export default function (name) {
-	let log = new Logger(config[name]);
-
-	if (process.env.NODE_ENV === 'development') {
-		log.on('log', (message) => {
-			console.log(message);
-		});
-	}
-
-	return log;
+	return new Logger(config[name]);
 }
