502758f501e7396d1eca14c4151b4bbbf9c3092e
Martin Kolárik <martin@kolarik.sk>
2015-08-05 12:25:19 +0200

    Stats: use cached data if something's missing

--- src/js/api/stats.js
+++ src/js/api/stats.js
@@ -4,7 +4,19 @@ import render from './stats/render';
 import logger from '../logger';
 
 let appLog = logger('app');
-let statsCache = {};
+let statsCache = JSON.stringify({
+	cedexis: {
+		decisions: [],
+		perfmap: [],
+		map: [],
+	},
+	dns: {
+		chart: [],
+	},
+	cdn: {
+		total: {},
+	},
+});
 
 updateData();
 setInterval(updateData, 3600000);
@@ -19,63 +31,76 @@ function updateData () {
 
 	request('http://dev.dakulov.com/jsdelivr/stats.php').then((body) => {
 		var data = JSON.parse(body);
+		var result = JSON.parse(statsCache); // Use previous data if something's missing.
 		appLog.debug('Got the following stats:');
 		appLog.debug(body);
 
-		// 1. Group by date.
-		// 2. Convert to arrays of [ date, hits ].
-		// 3. Remove today's (incomplete) stats.
-		data.dns.chart = _.map(_.groupBy(data.dns.chart, (entry) => {
-			let date = new Date(entry[0] * 1000);
-			return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
-		}), day => [ day[0][0], _.sum(day, entry => entry[1]) ]).slice(0, -1);
+		if (!_.isEmpty(data.dns.chart)) {
+			// 1. Group by date.
+			// 2. Convert to arrays of [ date, hits ].
+			// 3. Remove today's (incomplete) stats.
+			result.dns.chart = _.map(_.groupBy(data.dns.chart, (entry) => {
+				let date = new Date(entry[0] * 1000);
+				return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+			}), day => [ day[0][0], _.sum(day, entry => entry[1]) ]).slice(0, -1);
+		}
 
-		// 1. Group by date.
-		// 2. Convert to arrays of [ date, MaxCDN, CloudFlare, KeyCDN, Quantil ].
-		data.cedexis.decisions = _.map(_.groupBy(data.cedexis.decisions, 1), (decisions, date) => {
-			return [
-				Number(date),
-				_.find(decisions, decision => decision[0] === 'MaxCDN')[2],
-				_.find(decisions, decision => decision[0] === 'CloudFlare')[2],
-				_.find(decisions, decision => decision[0] === 'KeyCDN')[2],
-				_.get(_.find(decisions, decision => decision[0] === 'quantil'), 2, null),
-			];
-		});
+		if (!_.isEmpty(data.cedexis.decisions)) {
+			// 1. Group by date.
+			// 2. Convert to arrays of [ date, MaxCDN, CloudFlare, KeyCDN, Quantil ].
+			result.cedexis.decisions = _.map(_.groupBy(data.cedexis.decisions, 1), (decisions, date) => {
+				return [
+					Number(date),
+					_.find(decisions, decision => decision[0] === 'MaxCDN')[2],
+					_.find(decisions, decision => decision[0] === 'CloudFlare')[2],
+					_.find(decisions, decision => decision[0] === 'KeyCDN')[2],
+					_.get(_.find(decisions, decision => decision[0] === 'quantil'), 2, null),
+				];
+			});
+		}
 
-		// 1. Generate the charts and save them as images.
-		// 2. Use base64 encoding and include the images in the stats.
-		render(_.map(_.groupBy(data.cedexis.map, 0), (countryData) => {
-			return _.map(countryData, entry => [ entry[1], entry[2] ]).sort((a, b) => a[0] - b[0]);
-		})).then((images) => {
-			_.forEach(images, (image, index) => {
-				data.cedexis.map[index][2] = image.toString('base64');
+		if (!_.isEmpty(data.cedexis.map)) {
+			// 1. Generate the charts and save them as images.
+			// 2. Use base64 encoding and include the images in the stats.
+			render(_.map(_.groupBy(data.cedexis.map, 0), (countryData) => {
+				return _.map(countryData, entry => [ entry[1], entry[2] ]).sort((a, b) => a[0] - b[0]);
+			})).then((images) => {
+				_.forEach(images, (image, index) => {
+					result.cedexis.map[index][2] = image.toString('base64');
+				});
+
+				statsCache = JSON.stringify(result);
+				appLog.info('Chart images generated.');
+			}).catch((error) => {
+				appLog.err(error);
 			});
 
-			statsCache = JSON.stringify(data);
-			appLog.info('Chart images generated.');
-		}).catch((error) => {
-			appLog.err(error);
-		});
-
-		// 1. Group by country.
-		// 2. Reformat country names (Congo, The Democratic Republic of the -> The Democratic Republic of the Congo).
-		// 3. Sum hits per country.
-		data.cedexis.map = _.map(_.groupBy(data.cedexis.map, 0), (data, country) => {
-			return [ friendlyCountryName(country), _.sum(data, entry => entry[2]) ];
-		});
+			// 1. Group by country.
+			// 2. Reformat country names (Congo, The Democratic Republic of the -> The Democratic Republic of the Congo).
+			// 3. Sum hits per country.
+			result.cedexis.map = _.map(_.groupBy(data.cedexis.map, 0), (data, country) => {
+				return [ friendlyCountryName(country), _.sum(data, entry => entry[2]) ];
+			});
+		}
 
 		// 1. Group by country.
 		// 2. Reformat country names.
 		// 3. Get median of the values.
-		data.cedexis.perfmap = _.map(_.groupBy(data.cedexis.perfmap, 0), (data, country) => {
-			return [ friendlyCountryName(country), Math.round(median(_.map(data, entry => entry[2]))) ];
-		});
+		if (!_.isEmpty(data.cedexis.perfmap)) {
+			result.cedexis.perfmap = _.map(_.groupBy(data.cedexis.perfmap, 0), (data, country) => {
+				return [ friendlyCountryName(country), Math.round(median(_.map(data, entry => entry[2]))) ];
+			});
+		}
 
-		data.lastUpdate = Date.now();
-		statsCache = JSON.stringify(data);
+		result.cedexis.total = data.cedexis.total || result.cedexis.total;
+		result.cdn.total.hits = data.cdn.total.hits || result.cdn.total.hits;
+		result.cdn.total.traffic = data.cdn.total.traffic || result.cdn.total.traffic;
+		result.dns.total = data.dns.total || result.dns.total;
+		result.lastUpdate = Date.now();
+		statsCache = JSON.stringify(result);
 
 		appLog.info('Stats successfully updated.');
-		appLog.debug(data);
+		appLog.debug(result);
 	}).catch((error) => {
 		appLog.err(error);
 	});
