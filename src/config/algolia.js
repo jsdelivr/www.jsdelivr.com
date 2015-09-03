9d8afa0f24bf035868d25cbb8416c850a87ee777
Martin Kolárik <martin@kolarik.sk>
2015-09-03 14:02:57 +0200

    Make ALGOLIA_API_KEY optional

--- src/config/algolia.js
+++ src/config/algolia.js
@@ -1,5 +1,5 @@
 export default {
 	appID: '0UIFPQ3RGG',
 	readAccessToken: 'fd0fa679c0defa9861821fc129385ab5',
-	writeAccessToken: process.env.ALGOLIA_API_KEY,
+	writeAccessToken: process.env.ALGOLIA_API_KEY || 'development',
 };
