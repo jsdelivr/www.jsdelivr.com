dc45d0065b2ba7e96dd341298d1bc9346c0fc4bf
Martin Kolárik <martin@kolarik.sk>
2015-10-25 14:19:49 +0100

    Log errors from le_node instead of crashing the process

--- src/js/logger.js
+++ src/js/logger.js
@@ -2,5 +2,11 @@ import Logger from 'le_node';
 import config from '../config/logentries';
 
 export default function (name) {
-	return new Logger(config[name]);
+	let logger = new Logger(config[name]);
+
+	logger.on('error', (error) => {
+		console.error(error);
+	});
+
+	return logger;
 }
