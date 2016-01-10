7d0fbeaee65a773c650d082051c9b5c19dc059e5
Martin Kolárik <martin@kolarik.sk>
2016-01-10 01:33:40 +0100

    Add a simple gobble file revision transformer

--- gobblefile.js
+++ gobblefile.js
@@ -1,5 +1,8 @@
+var path = require('path');
+var crypto = require('crypto');
 var gobble = require('gobble');
 
+var hashes = Object.create(null);
 var app = gobble([
 		gobble('src/public/js')
 			.observeIf(gobble.env() === 'development', 'jscs')
@@ -24,11 +27,14 @@ module.exports = gobble([
 		space_before_conditional: true,
 		end_with_newline: true,
 	}),
-	app.transform('uglifyjs', { ext: '.min.js' }),
+	app
+		.transform('uglifyjs', { ext: '.min.js' })
+		.observe(hash),
 	gobble('src/public/less')
 		.transform('less', { src: 'app.less', dest: 'app.css', strictMath: true })
 		.transform('clean-css', { ext: '.min.css' })
-		.moveTo('public/css'),
+		.moveTo('public/css')
+		.observe(hash),
 	gobble('src')
 		.exclude('public/**')
 		.observeIf(gobble.env() === 'development', 'jscs')
@@ -37,6 +43,31 @@ module.exports = gobble([
 		.include( '**/*.js' ),
 	gobble('src/public/img')
 		.moveTo('public/img'),
-	gobble('src/views')
+	gobble([ 'src/views', 'src/public/js', 'src/public/less' ])
+		.include('**/*.html')
+		.transform(updateRefs)
 		.moveTo('views'),
 ]);
+
+function hash (inputDir) {
+	gobble.sander.lsrSync(inputDir).map(function (file) {
+		var code = gobble.sander.readFileSync(path.join(inputDir, file)).toString();
+		var fKey = '/' + path.relative('public', file).replace(/\\/g, '/');
+
+		hashes[fKey] = crypto.createHash('sha256').update(code, 'utf8').digest('hex').substr(0, 8);
+	});
+
+	return Promise.resolve();
+}
+
+function updateRefs (inputDir, outputDir, options) {
+	gobble.sander.lsrSync(inputDir).map(function (file) {
+		var code = gobble.sander.readFileSync(path.join(inputDir, file)).toString().replace(/<<UPDATE\((.*?)\)>>/g, function ($0, $1) {
+			return $1 + (hashes[$1] ? '?v=' + hashes[$1] : '');
+		});
+
+		gobble.sander.writeFileSync(path.join(outputDir, file), code);
+	});
+
+	return Promise.resolve(options);
+}
