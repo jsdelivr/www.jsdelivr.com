f378ff4502d48d3c740b84c08fe38a63cef21164
Martin Kolárik <martin@kolarik.sk>
2017-08-30 13:08:25 +0200

    Use SOURCE_VERSION var on Heroku (fixes #101)

--- src/middleware/render/index.js
+++ src/middleware/render/index.js
@@ -5,7 +5,7 @@ const rcu = require('rcu');
 const childProcess = require('child_process');
 
 const componentCache = new Map();
-let commitHash;
+let commitHash = process.env.SOURCE_VERSION || '';
 
 try {
 	commitHash = childProcess.execSync('git log -1 "--format=%H"', { encoding: 'utf8' }).trim();
