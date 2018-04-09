ba351d8a7ef1b9905c57e6af69691e15ac6a33db
Martin Kolárik <martin@kolarik.sk>
2018-04-09 19:30:05 +0200

    Add @types

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/less/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/fs-extra-promise/index.d.ts" />
-/// <reference path="modules/fs-extra/index.d.ts" />
-/// <reference path="modules/koa-router/index.d.ts" />
-/// <reference path="modules/koa/index.d.ts" />
