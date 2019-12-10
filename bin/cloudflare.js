2cbcba3cc479ee60370884581d48496f180fecd3
SukkaW <isukkaw@gmail.com>
2019-12-11 02:27:38 +0800

    Add Cloudflare data center list fetcher

---
+++ bin/cloudflare.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const fetch = require('node-fetch-retry');
+const parseCsv = require('csv-parse/lib/sync');
+
+fetch('https://www.cloudflare.com/data/current-pops.csv')
+    .then(res => res.text())
+    .then(text => {
+        return parseCsv(text, {
+            columns: true,
+            skip_empty_lines: true
+        });
+    })
+    .then(data => {
+        const result = [];
+
+        data.forEach((dc) => {
+            if (dc.region !== 'CN') {
+                result.push(`Cloudflare - ${dc.city.split(', ')[0]}`)
+            }
+        });
+
+        return result;
+    })
+    .then(result => {
+        result.forEach((i) => {
+            console.log(i);
+        })
+    })
