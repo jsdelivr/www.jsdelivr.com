30d514f36d12f869db91cac1582c248fe3e1db00
SukkaW <isukkaw@gmail.com>
2019-12-11 02:55:24 +0800

    Makes eslint happy

--- bin/cloudflare.js
+++ bin/cloudflare.js
@@ -2,27 +2,28 @@
 const fetch = require('node-fetch-retry');
 const parseCsv = require('csv-parse/lib/sync');
 
+const result = [];
+
 fetch('https://www.cloudflare.com/data/current-pops.csv')
-    .then(res => res.text())
-    .then(text => {
-        return parseCsv(text, {
-            columns: true,
-            skip_empty_lines: true
-        });
-    })
-    .then(data => {
-        const result = [];
+	.then((res) => { return res.text(); })
+	.then((text) => {
+		return parseCsv(text, {
+			columns: true,
+			skip_empty_lines: true,
+		});
+	})
+	.then((data) => {
+		data.forEach((dc) => {
+			if (dc.region !== 'CHINA') {
+				result.push(`Cloudflare - ${dc.city.split(', ')[0]}`);
+			}
+		});
 
-        data.forEach((dc) => {
-            if (dc.region !== 'CHINA') {
-                result.push(`Cloudflare - ${dc.city.split(', ')[0]}`)
-            }
-        });
-
-        return result.sort();
-    })
-    .then(result => {
-        result.forEach((i) => {
-            console.log(i);
-        })
-    })
+		return result.sort();
+	})
+	.then((result) => {
+		result.forEach((i) => {
+			console.log(i);
+		});
+	})
+	.catch(() => {});
