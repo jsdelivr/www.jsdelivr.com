212eba0a7e010ce4b0e8adb95ab0036e28815614
xbpcb <ch.kyrill@gmail.com>
2021-11-22 20:35:16 +0200

    Fix purge url validation

--- src/public/js/utils/is-url-valid.js
+++ src/public/js/utils/is-url-valid.js
@@ -7,7 +7,7 @@ module.exports = (url) => {
 	// OR ip (v4) address
 	'((\\d{1,3}\\.){3}\\d{1,3}))'+
 	 // port and path
-	'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
+	'(\\:\\d+)?(\\/[-a-z\\d%_.~+@]*)*'+
 	 // query string
 	'(\\?[;&a-z\\d%_.~+=-]*)?'+
 	 // fragment locator
@@ -15,5 +15,3 @@ module.exports = (url) => {
 
 	return pattern.test(url);
 };
-
-// '^(https?:\\/\\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|((\\d{1,3}\\.){3}\\d{1,3}))(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$'
