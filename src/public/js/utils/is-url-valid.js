82fdb493ce0ab4b9e8cadf62e7b514b92c8f4702
xbpcb <ch.kyrill@gmail.com>
2021-11-23 16:33:21 +0200

    Fix linter issues

--- src/public/js/utils/is-url-valid.js
+++ src/public/js/utils/is-url-valid.js
@@ -1,17 +1,10 @@
 module.exports = (url) => {
-	const pattern = new RegExp(
-	// protocol
-	'^(https?:\\/\\/)?'+
-	// domain name
-	'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
-	// OR ip (v4) address
-	'((\\d{1,3}\\.){3}\\d{1,3}))'+
-	 // port and path
-	'(\\:\\d+)?(\\/[-a-z\\d%_.~+@]*)*'+
-	 // query string
-	'(\\?[;&a-z\\d%_.~+=-]*)?'+
-	 // fragment locator
-	'(\\#[-a-z\\d_]*)?$','i');
+	let pattern = new RegExp('^(https?:\\/\\/)?'// protocol
+		+ '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'// domain name
+		+ '((\\d{1,3}\\.){3}\\d{1,3}))'// OR ip (v4) address
+		+ '(\\:\\d+)?(\\/[-a-z\\d%_.~+@]*)*'// port and path
+		+ '(\\?[;&a-z\\d%_.~+=-]*)?'// query string
+		+ '(\\#[-a-z\\d_]*)?$', 'i');// fragment locator
 
 	return pattern.test(url);
 };
