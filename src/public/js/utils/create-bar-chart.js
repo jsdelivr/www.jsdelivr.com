6c787414dde479dc2b7388fc1f9e8c399059f00b
xbpcb <ch.kyrill@gmail.com>
2022-02-07 18:12:11 +0200

    Fix linter issues

--- src/public/js/utils/create-bar-chart.js
+++ src/public/js/utils/create-bar-chart.js
@@ -132,7 +132,6 @@ function createBarChart (
 					display: false,
 				},
 				tooltip: {
-					enabled: true,
 					enabled: !chartSettings.useExternalTooltip && true,
 					external: chartSettings.useExternalTooltip ? externalTooltip : null,
 					bodyColor: '#fff',
