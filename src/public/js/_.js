9cc18b10fff13ca337a548a4abd2e5aa6f5ca0cf
xbpcb <ch.kyrill@gmail.com>
2022-02-11 19:55:23 +0200

    Create autoConvertBytesToUnits on nFormatter base

--- src/public/js/_.js
+++ src/public/js/_.js
@@ -179,4 +179,22 @@ module.exports = {
 
 		return Math.round(bytesAmount / unitsBase);
 	},
+	autoConvertBytesToUnits (num) {
+		let lookup = [
+			{ value: 1, symbol: '' },
+			{ value: 1e3, symbol: 'K' },
+			{ value: 1e6, symbol: 'M' },
+			{ value: 1e9, symbol: 'G' },
+			{ value: 1e12, symbol: 'T' },
+			{ value: 1e15, symbol: 'P' },
+			{ value: 1e18, symbol: 'E' },
+		];
+		let rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+
+		let item = lookup.slice().reverse().find((item) => {
+			return num >= item.value;
+		});
+
+		return item ? (num / item.value).toFixed(1).replace(rx, '$1') + item.symbol : '0';
+	},
 };
