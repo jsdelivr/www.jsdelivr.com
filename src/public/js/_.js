d3c5131778b835ceef63038ffdf05a746f29e419
xbpcb <ch.kyrill@gmail.com>
2022-02-14 20:01:21 +0200

    Add makeHTTPRequest method to _

--- src/public/js/_.js
+++ src/public/js/_.js
@@ -197,4 +197,25 @@ module.exports = {
 
 		return item ? (num / item.value).toFixed(1).replace(rx, '$1') + item.symbol : '0';
 	},
+	makeHTTPRequest (obj) {
+		return new Promise((resolve, reject) => {
+			let xhr = new XMLHttpRequest();
+			xhr.open(obj.method || 'GET', obj.url);
+
+			if (obj.headers) {
+				Object.keys(obj.headers).forEach(key => xhr.setRequestHeader(key, obj.headers[key]));
+			}
+
+			xhr.onload = () => {
+				if (xhr.status >= 200 && xhr.status < 300) {
+					resolve(xhr.response);
+				} else {
+					reject(xhr.statusText);
+				}
+			};
+
+			xhr.onerror = () => reject(xhr.statusText);
+			xhr.send(obj.body);
+		});
+	},
 };
