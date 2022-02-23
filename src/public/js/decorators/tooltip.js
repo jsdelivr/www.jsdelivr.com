91736f954e70a8374cdd1ad85f706658e5e69e60
xbpcb <ch.kyrill@gmail.com>
2022-02-23 13:42:26 +0200

    Scaffold vanilla js tooltip version

--- src/public/js/decorators/tooltip.js
+++ src/public/js/decorators/tooltip.js
@@ -1,14 +1,42 @@
-module.exports = (node, title, placement = 'top', trigger = 'hover', container = 'body') => {
-	let $node = $(node).tooltip({
-		title,
-		placement,
-		trigger,
-		container,
-	});
+module.exports = (
+	node,
+	content,
+	elementName = 'div',
+	className,
+	offsetX,
+	offsetY
+) => {
+	let tooltip, handlers, eventName;
+
+	handlers = {
+		mouseover () {
+			tooltip = document.createElement(elementName);
+			tooltip.className = `ractive-tooltip${className ? ` ${className}` : ''}`;
+			tooltip.textContent = content;
+			node.parentNode.insertBefore(tooltip, node);
+		},
+		mousemove (event) {
+			tooltip.style.left = `${offsetX ? offsetX : event.clientX}px`;
+			tooltip.style.top = `${offsetY ? offsetY : event.clientY - tooltip.clientHeight - 20}px`;
+		},
+		mouseleave () {
+			tooltip.parentNode.removeChild(tooltip);
+		},
+	};
+
+	for (eventName in handlers) {
+		if (Object.prototype.hasOwnProperty.call(handlers, eventName)) {
+			node.addEventListener(eventName, handlers[eventName], false);
+		}
+	}
 
 	return {
 		teardown () {
-			$node.tooltip('destroy');
+			for (eventName in handlers) {
+				if (Object.prototype.hasOwnProperty.call(handlers, eventName)) {
+					node.removeEventListener(eventName, handlers[eventName], false);
+				}
+			}
 		},
 	};
 };
