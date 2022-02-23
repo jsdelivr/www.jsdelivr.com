0884665d2d88db06a1f89a061a9c2c7c15e1f00d
xbpcb <ch.kyrill@gmail.com>
2022-02-23 15:14:00 +0200

    Scaffold ractive-tooltip position styles

--- src/public/js/decorators/tooltip.js
+++ src/public/js/decorators/tooltip.js
@@ -1,22 +1,39 @@
 module.exports = (
 	node,
 	content,
+	position = 'bottom',
 	elementName = 'div',
 	className,
 	offsetX,
 	offsetY
 ) => {
 	let tooltip, handlers, eventName;
+	let getPositionClass = (position) => {
+		let resClass;
+
+		switch(position) {
+			case 'top':
+				resClass = 'ractive-tooltip-top';break;
+			case 'left':
+				resClass = 'ractive-tooltip-left';break;
+			case 'right':
+				resClass = 'ractive-tooltip-right';break;
+			default:
+				resClass = 'ractive-tooltip-bottom';
+		}
+
+		return resClass;
+	};
 
 	handlers = {
 		mouseover () {
 			tooltip = document.createElement(elementName);
-			tooltip.className = `ractive-tooltip${className ? ` ${className}` : ''}`;
+			tooltip.className = `ractive-tooltip ${getPositionClass(position)}${className ? ` ${className}` : ''}`;
 			tooltip.textContent = content;
 			node.parentNode.insertBefore(tooltip, node);
 		},
 		mousemove (event) {
-			tooltip.style.left = `${offsetX ? offsetX : event.clientX}px`;
+			tooltip.style.left = `${offsetX ? offsetX : event.clientX - Math.round(tooltip.offsetWidth / 2)}px`;
 			tooltip.style.top = `${offsetY ? offsetY : event.clientY - tooltip.clientHeight - 20}px`;
 		},
 		mouseleave () {
