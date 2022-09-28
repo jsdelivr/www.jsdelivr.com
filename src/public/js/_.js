a60bead5a4c4c62654f77fda5085b0d51982b81b
xbpcb <ch.kyrill@gmail.com>
2022-09-28 13:47:30 +0300

    Scaffold util func to get month days amount

--- src/public/js/_.js
+++ src/public/js/_.js
@@ -359,5 +359,25 @@ module.exports = {
 		} else {
 			return false;
 		}
+	},
+
+	getAmountOfDaysByMonth (month, year) {
+		switch (month) {
+			case '01':
+			case '03':
+			case '05':
+			case '07':
+			case '08':
+			case '10':
+			case '12':
+				return 31;
+			case '04':
+			case '06':
+			case '09':
+			case '11':
+				return 30;
+			case '02':
+				return this.checkIfYearIsLeap(year) ? 29 : 28;
+		}
 	}
 };
