563bc014d18219cd7763347de62b1bf66457e8cd
xbpcb <ch.kyrill@gmail.com>
2022-10-02 19:37:57 +0300

    CustomCdnOssProj: fix year-week last month x-axis tick

--- src/public/js/_.js
+++ src/public/js/_.js
@@ -547,13 +547,14 @@ module.exports = {
 
 		if (groupBy === 'week') {
 			let tempCurrMonth = labels[0][1];
+			let lastPeriodMonth = labels[labels.length - 1][1];
 
 			formattedLabels = labels.map((label, idx) => {
 				switch (true) {
 					case screen.width >= 768:
 						if (idx === 0 || idx === labels.length - 1) {
 							return label.slice(1, 3);
-						} else if (label[1] !== tempCurrMonth) {
+						} else if (label[1] !== tempCurrMonth && label[1] !== lastPeriodMonth) {
 							tempCurrMonth = label[1];
 
 							return label.slice(1, 2);
