e2f4cc438fa826984d5198f4f462e931fa193262
xbpcb <ch.kyrill@gmail.com>
2023-02-27 17:10:28 +0200

    GP-cli: scaffold page

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -29,6 +29,7 @@ const cCustomCdnOss = require('../../views/pages/oss-cdn.html');
 const cCustomCdnOssProject = require('../../views/pages/_oss-cdn-project.html');
 const cDocumentation = require('../../views/pages/documentation.html');
 const cGlobalping = require('../../views/pages/globalping.html');
+const cGlobalpingCli = require('../../views/pages/globalping-cli.html');
 
 Ractive.DEBUG = location.hostname === 'localhost';
 
@@ -99,6 +100,7 @@ app.router.addRoute('/oss-cdn', cCustomCdnOss);
 app.router.addRoute('/oss-cdn/:name', cCustomCdnOssProject);
 app.router.addRoute('/documentation', cDocumentation);
 app.router.addRoute('/globalping', cGlobalping);
+app.router.addRoute('/globalping-cli', cGlobalpingCli);
 app.router.addRoute('/(.*)', () => { location.href = '/'; });
 
 _.onDocumentReady(() => {
