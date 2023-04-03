44c43366b7fcc67deb15b29465aba6e0d06294c3
xbpcb <ch.kyrill@gmail.com>
2023-04-03 17:41:15 +0300

    ValidateURL util: mod regex

--- src/assets/js/utils/validate-url.js
+++ src/assets/js/utils/validate-url.js
@@ -1,6 +1,6 @@
 module.exports = {
 	test: (url) => {
-		let urlPattern = new RegExp('^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9])).([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}.[a-zA-Z]{2,3})$', 'i');
+		let urlPattern = new RegExp('^(?!-)[A-Za-z0-9-]+([\-\.]{1}[a-z0-9]+)*\.[A-Za-z]{2,6}$', 'i');
 
 		return urlPattern.test(url);
 	},
