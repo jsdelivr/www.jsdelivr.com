627c85b8749572aae0631529fa7478b83dcd2412
Martin Kolárik <martin@kolarik.sk>
2023-04-23 16:19:52 +0200

    Change transactionSampleRate to 0.1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 // This needs to run before any require() call.
-global.apmClient = require('elastic-apm-node').start({
-	active: process.env.NODE_ENV === 'production',
-	serviceName: 'jsdelivr-website',
-	serviceVersion: require('../package.json').version,
-	logLevel: 'fatal',
-	centralConfig: false,
-	captureExceptions: false,
-	captureSpanStackTraces: false,
-	captureErrorLogStackTraces: 'always',
-	ignoreUrls: [ '/favicon.ico', '/heartbeat', '/amp_preconnect_polyfill_404_or_other_error_expected._Do_not_worry_about_it' ],
-	errorOnAbortedRequests: true,
-	abortedErrorThreshold: 30,
-	transactionSampleRate: .2,
-});
-
+global.apmClient = require('elastic-apm-node').start({});
 global.apmClient.addTransactionFilter(require('elastic-apm-utils').apm.transactionFilter());
 require('./lib/startup');
 
