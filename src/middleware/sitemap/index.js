35819b9323e604829dc719a139d5d062fe3183bc
xbpcb <ch.kyrill@gmail.com>
2023-10-30 19:11:39 +0200

    Add new cdns pages to sitemap

--- src/middleware/sitemap/index.js
+++ src/middleware/sitemap/index.js
@@ -23,6 +23,8 @@ module.exports = async (ctx) => {
 		'oss-cdn/ghost',
 		'oss-cdn/musescore',
 		'oss-cdn/pyodide',
+		'oss-cdn/fontsource',
+		'oss-cdn/yocto-sstate',
 	);
 
 	if (ctx.params.page === 'index') {
