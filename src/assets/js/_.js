746edf2148c99a9446d4845db26e41c677d4f387
xbpcb <ch.kyrill@gmail.com>
2024-03-06 15:38:40 +0200

    Fix

--- src/assets/js/_.js
+++ src/assets/js/_.js
@@ -1221,7 +1221,7 @@ module.exports = {
 		let { timings = [] } = result;
 
 		if (testType === 'ping') {
-			lastTiming = timings[timings.length - 1] ? timings[timings.length - 1].rtt : NO_PROBE_TIMING_VALUE;// TODO: 592 - what value we should return in this case?
+			lastTiming = timings[timings.length - 1] ? timings[timings.length - 1].rtt : PROBE_NO_TIMING_VALUE;// TODO: 592 - what value we should return in this case?
 		}
 
 		return lastTiming;
@@ -1370,7 +1370,7 @@ module.exports = {
 			extraValues,
 			fullText: PROBE_NO_TIMING_VALUE,
 			isFailed: true,
-			lastTiming: NO_PROBE_TIMING_VALUE,
+			lastTiming: PROBE_NO_TIMING_VALUE,
 		};
 	},
 
