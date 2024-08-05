fb122d5aa0d96ca8bc4fd501062bd77286fa1436
Pavel Kopecký <pavel.kopecky@icloud.com>
2024-08-05 17:18:41 +0200

    separate og generating files

--- src/middleware/open-graph/index.js
+++ src/middleware/open-graph/index.js
@@ -10,6 +10,15 @@ const entities = require('entities');
 const FontsProcessor = require('./fonts');
 const algoliaNode = require('../../lib/algolia-node');
 const got = require('../../lib/got');
+const { readFileSync } = require('fs');
+
+const generatePingOG = require('./lib/generate-ping-og');
+const generateMtrOG = require('./lib/generate-mtr-og');
+const generateTracerouteOG = require('./lib/generate-traceroute-og');
+const generateHttpOG = require('./lib/generate-http-og');
+const generateDnsOG = require('./lib/generate-dns-og');
+
+const globalpingOG = readFileSync(path.resolve(__dirname, '../../assets/img/og-globalping.png'));
 
 const API_HOST = 'https://data.jsdelivr.com';
 const GLOBALPING_API_HOST = 'https://api.globalping.io';
@@ -240,442 +249,15 @@ module.exports = async (ctx) => {
 	}
 };
 
-const formatNumber = (number) => {
-	if (number > 0 && number < 1) {
-		return '1';
-	}
-
-	return number.toFixed(0);
-};
-
-const makeRangeString = (low, high) => {
-	return low === high ? low : low + ' - ' + high;
-};
-
-// Based on https://en.wikipedia.org/wiki/Interquartile_range#Outliers:~:text=be%20indicated%20here.-,Outliers,-%5Bedit%5D
-const removeOutliers = (array) => {
-	let len = array.length;
-
-	if (len < 10) {
-		return array;
-	}
-
-	{
-		let q1 = array[Math.floor(len * 0.25)];
-		let q3 = array[Math.floor(len * 0.75)];
-		let iqr = q3 - q1;
-
-		let upperBound = q3 + 1.5 * iqr;
-		let lowerBound = q1 - 1.5 * iqr;
-
-		return array.filter(val => (val >= lowerBound) && (val <= upperBound));
-	}
-};
-
-const getNumberRange = (array) => {
-	let filtered = removeOutliers(array.filter(val => typeof val === 'number'));
-	return makeRangeString(formatNumber(Math.min(...filtered)), formatNumber(Math.max(...filtered)));
-};
-
-const getLocationString = (locations) => {
-	let locationStr = locations.map((location) => {
-		return Object.values(location).join('+');
-	}).join(', ');
-
-	return truncateString(locationStr, 'Lexend SemiBold', 32, 550).text;
-};
-
-const generatePingOG = async (ctx, data) => {
-	let viableData = data.results.filter(obj => obj.result.stats?.avg);
-
-	let location = getLocationString(data.locations);
-	let locationWidth = fontsProcessor.computeWidth(location, 'Lexend SemiBold', 32, -0.6);
-	let probes = data.results.length + ' probe' + ((data.results.length > 1) && 's');
-
-	let svgData;
-
-	if (viableData.length === 0) {
-		svgData = {
-			location,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			probes,
-			locationWidth,
-			failure: true,
-		};
-	} else {
-		let latRange = getNumberRange(viableData.map(obj => obj.result.stats?.avg)) + ' ms';
-		let lossRange = getNumberRange(viableData.map(obj => obj.result.stats?.loss)) + ' %';
-
-		let latencyWidth = fontsProcessor.computeWidth(latRange, 'Lexend SemiBold', 32, -0.6);
-		let packetLossWidth = fontsProcessor.computeWidth(lossRange, 'Lexend SemiBold', 32, -0.6);
-
-		let lossOffset = Math.max(latencyWidth + 146, 347);
-
-		svgData = {
-			latRange,
-			lossRange,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			location,
-			latencyWidth,
-			packetLossWidth,
-			probes,
-			locationWidth,
-			lossOffset,
-		};
-	}
-
-	return ctx.render('globalping-og/og-image-ping.svg', svgData);
-};
-
-const generateMtrOG = async (ctx, data) => {
-	let viableData = data.results.filter(obj => obj.result.hops?.at(-1).stats?.avg);
-
-	let location = getLocationString(data.locations);
-	let locationWidth = fontsProcessor.computeWidth(location, 'Lexend SemiBold', 32, -0.6);
-	let probes = data.results.length + ' probe' + ((data.results.length > 1) && 's');
-
-	let svgData;
-
-	if (viableData.length === 0) {
-		svgData = {
-			location,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			probes,
-			locationWidth,
-			failure: true,
-		};
-	} else {
-		let latRange = getNumberRange(viableData.map(obj => obj.result.hops?.at(-1).stats?.avg)) + ' ms';
-		let lossRange = getNumberRange(viableData.map(obj => obj.result.hops?.at(-1).stats?.loss)) + ' %';
-		let hopRange = getNumberRange(viableData.map(obj => obj.result.hops?.length));
-
-		let latencyWidth = fontsProcessor.computeWidth(latRange, 'Lexend SemiBold', 32, -0.6);
-		let packetLossWidth = fontsProcessor.computeWidth(lossRange, 'Lexend SemiBold', 32, -0.6);
-		let hopRangeWidth = fontsProcessor.computeWidth(hopRange, 'Lexend SemiBold', 32, -0.6);
-
-		let lossOffset = Math.max(latencyWidth + 146, 347);
-		let hopOffset = Math.max(lossOffset + packetLossWidth + 60, lossOffset + 220);
-
-		svgData = {
-			latRange,
-			lossRange,
-			hopRange,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			location,
-			latencyWidth,
-			packetLossWidth,
-			probes,
-			locationWidth,
-			lossOffset,
-			hopRangeWidth,
-			hopOffset };
-	}
-
-	return ctx.render('globalping-og/og-image-mtr.svg', svgData);
-};
-
-const generateTracerouteOG = async (ctx, data) => {
-	let viableData = data.results.filter(obj => obj.result.hops?.at(-1).timings.length);
-
-	let location = getLocationString(data.locations);
-	let locationWidth = fontsProcessor.computeWidth(location, 'Lexend SemiBold', 32, -0.6);
-	let probes = data.results.length + ' probe' + ((data.results.length > 1) && 's');
-
-	let svgData;
-
-	if (viableData.length === 0) {
-		svgData = {
-			location,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			probes,
-			locationWidth,
-			failure: true,
-		};
-	} else {
-		let latRange = getNumberRange(viableData.map(obj => _.mean(obj.result.hops?.at(-1).timings.map(timing => timing.rtt)))) + ' ms';
-		let hopRange = getNumberRange(viableData.map(obj => obj.result.hops?.length));
-
-		let latencyWidth = fontsProcessor.computeWidth(latRange, 'Lexend SemiBold', 32, -0.6);
-		let hopRangeWidth = fontsProcessor.computeWidth(hopRange, 'Lexend SemiBold', 32, -0.6);
-
-		let hopOffset = Math.max(latencyWidth + 146, 347);
-
-		svgData = {
-			latRange,
-			hopRange,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			location,
-			latencyWidth,
-			probes,
-			locationWidth,
-			hopRangeWidth,
-			hopOffset };
-	}
-
-	return ctx.render('globalping-og/og-image-traceroute.svg', svgData);
-};
-
-function getStatusCodes (array) {
-	let statusCountMap = {};
-	let filtered = array.filter(val => typeof val.result?.statusCode === 'number');
-
-	let err = array.length - filtered.length;
-
-	filtered.forEach((item) => {
-		let code = item.result.statusCode?.toString();
-
-		if (code in statusCountMap) {
-			statusCountMap[code]++;
-		} else {
-			statusCountMap[code] = 1;
-		}
-	});
-
-	let statusCountArray = Object.entries(statusCountMap);
-
-	if (err) {
-		statusCountArray.push([ 'Error', err ]);
-	}
-
-	statusCountArray.sort((a, b) => b[1] - a[1]);
-
-	return statusCountArray;
-}
-
-const generateHttpOG = async (ctx, data) => {
-	let viableData = data.results.filter(obj => obj.result?.timings?.total && obj.result?.statusCode);
-
-	let location = getLocationString(data.locations);
-	let method = data.measurementOptions?.request?.method ?? 'HEAD';
-	let probes = data.results.length + ' probe' + ((data.results.length > 1) && 's');
-
-	let path = data.measurementOptions?.request?.path ?? '';
-	let query = data.measurementOptions?.request?.query ?? '';
-	let completePathString = '/' + path.replace(/^\//, '') + (query && '?' + query.replace(/^\?/, ''));
-	let finalPath = truncateString(completePathString, 'Lexend SemiBold', 30, 800).text;
-
-	let pathWidth = fontsProcessor.computeWidth(finalPath, 'Lexend SemiBold', 30, -0.6);
-	let methodWidth = fontsProcessor.computeWidth(method, 'Lexend SemiBold', 34, -0.6);
-	let locationWidth = fontsProcessor.computeWidth(location, 'Lexend SemiBold', 32, -0.6);
-
-	let svgData;
-
-	if (viableData.length === 0) {
-		svgData = {
-			location,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			probes,
-			locationWidth,
-			pathWidth,
-			method,
-			methodWidth,
-			failure: true,
-			path: finalPath,
-		};
-	} else {
-		let timeRange = getNumberRange(viableData.map(obj => obj.result.timings?.total)) + ' ms';
-		let statusCodes = getStatusCodes(viableData);
-		let usedStatusCodes = statusCodes.slice(0, 3);
-
-		let remaining = null;
-		let timeOffset = 0;
-
-		if (statusCodes.length > usedStatusCodes.length) {
-			remaining = { count: statusCodes.length - usedStatusCodes.length, offset: 0 };
-		}
-
-		usedStatusCodes.forEach((code, index) => {
-			let width = fontsProcessor.computeWidth(`${code[0]} `, 'Lexend SemiBold', 32, -0.6) + fontsProcessor.computeWidth(`(${code[1]})`, 'Lexend Regular', 30, -0.6);
-			let offset;
-
-			if (index === 0) {
-				offset = 88.5;
-			} else {
-				offset = usedStatusCodes[index - 1][2] + usedStatusCodes[index - 1][3] + 42;
-			}
-
-			code.push(offset);
-			code.push(width);
-
-			if (index === 2) {
-				remaining.offset = offset + width + 42;
-			}
-		});
-
-		if (remaining) {
-			timeOffset = remaining.offset + fontsProcessor.computeWidth(`+${remaining.count}`, 'Lexend Regular', 32, -0.6) + 32;
-		} else {
-			timeOffset = usedStatusCodes.at(-1)[2] + usedStatusCodes.at(-1)[3] + 32;
-		}
-
-		let timeWidth = fontsProcessor.computeWidth(timeRange, 'Lexend SemiBold', 32, -0.6);
-
-		timeOffset = Math.max(timeOffset, 470);
-
-		svgData = {
-			method,
-			methodWidth,
-			statusCodes: usedStatusCodes,
-			timeRange,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			location,
-			timeWidth,
-			statusCodesWidth: 500,
-			probes,
-			locationWidth,
-			timeOffset,
-			path: finalPath,
-			pathWidth,
-			remaining,
-		};
-	}
-
-	return ctx.render('globalping-og/og-image-http.svg', svgData);
-};
-
-const generateTraceDnsOG = async (ctx, data) => {
-	let viableData = data.results.filter(obj => obj.result.status === 'finished' && obj.result.hops.length);
-
-	console.log('\n\na\n\n');
-
-	let location = getLocationString(data.locations);
-	let probes = data.results.length + ' probe' + ((data.results.length > 1) && 's');
-
-	console.log('\n\nb\n\n');
-
-	let locationWidth = fontsProcessor.computeWidth(location, 'Lexend SemiBold', 32, -0.6);
-
-	let svgData;
-
-	if (viableData.length === 0) {
-		svgData = {
-			location,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			probes,
-			locationWidth,
-			failure: true,
-		};
-	} else {
-		console.log('\n\nc\n\n');
-		let timeRange = getNumberRange(viableData.map(obj => obj.result.hops.at(-1).timings.total)) + ' ms';
-		let answersRange = getNumberRange(viableData.map(obj => obj.result.hops.at(-1).answers.length));
-		console.log('\n\nd\n\n');
-
-		let timeWidth = fontsProcessor.computeWidth(timeRange, 'Lexend SemiBold', 32, -0.6);
-		let answersWidth = fontsProcessor.computeWidth(answersRange, 'Lexend SemiBold', 32, -0.6);
-
-		let answersOffset = Math.max(timeWidth + 146, 347);
-
-		svgData = {
-			timeRange,
-			answersRange,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			location,
-			probes,
-			timeWidth,
-			locationWidth,
-			answersWidth,
-			answersOffset,
-		};
-	}
-
-	return ctx.render('globalping-og/og-image-dns.svg', svgData);
-};
-
-
-const generateDnsOG = async (ctx, data) => {
-	if (data.measurementOptions?.trace) {
-		return generateTraceDnsOG(ctx, data);
-	}
-
-	let viableData = data.results.filter(obj => obj.result?.timings?.total && obj.result?.statusCode);
-
-	let location = getLocationString(data.locations);
-	let probes = data.results.length + ' probe' + ((data.results.length > 1) && 's');
-
-	let locationWidth = fontsProcessor.computeWidth(location, 'Lexend SemiBold', 32, -0.6);
-
-	let svgData;
-
-	if (viableData.length === 0) {
-		svgData = {
-			location,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			probes,
-			locationWidth,
-			pathWidth,
-			method,
-			methodWidth,
-			failure: true,
-			path: finalPath,
-		};
-	} else {
-		let timeRange = getNumberRange(viableData.map(obj => obj.result.timings?.total)) + ' ms';
-		let statusCodes = getStatusCodes(viableData);
-		let usedStatusCodes = statusCodes.slice(0, 3);
-
-		let remaining = null;
-		let timeOffset = 0;
-
-		if (statusCodes.length > usedStatusCodes.length) {
-			remaining = { count: statusCodes.length - usedStatusCodes.length, offset: 0 };
-		}
-
-		usedStatusCodes.forEach((code, index) => {
-			let width = fontsProcessor.computeWidth(`${code[0]} `, 'Lexend SemiBold', 32, -0.6) + fontsProcessor.computeWidth(`(${code[1]})`, 'Lexend Regular', 30, -0.6);
-			let offset;
-
-			if (index === 0) {
-				offset = 88.5;
-			} else {
-				offset = usedStatusCodes[index - 1][2] + usedStatusCodes[index - 1][3] + 42;
-			}
-
-			code.push(offset);
-			code.push(width);
-
-			if (index === 2) {
-				remaining.offset = offset + width + 42;
-			}
-		});
-
-		if (remaining) {
-			timeOffset = remaining.offset + fontsProcessor.computeWidth(`+${remaining.count}`, 'Lexend Regular', 32, -0.6) + 32;
-		} else {
-			timeOffset = usedStatusCodes.at(-1)[2] + usedStatusCodes.at(-1)[3] + 32;
-		}
-
-		let timeWidth = fontsProcessor.computeWidth(timeRange, 'Lexend SemiBold', 32, -0.6);
-
-		timeOffset = Math.max(timeOffset, 470);
-
-		svgData = {
-			method,
-			methodWidth,
-			statusCodes: usedStatusCodes,
-			timeRange,
-			target: truncateString(data.target, 'Lexend SemiBold', 72, 750).text,
-			location,
-			timeWidth,
-			statusCodesWidth: 500,
-			probes,
-			locationWidth,
-			timeOffset,
-			path: finalPath,
-			pathWidth,
-			remaining,
-		};
-	}
-
-	return ctx.render('globalping-og/og-image-http.svg', svgData);
-};
-
 module.exports.globalping = async (ctx) => {
 	try {
 		let data = await fetchGlobalpingStats(ctx.params.id.split(',')[0].split('.')[0]);
 
 		if (!data || data.status !== 'finished') {
-			// TODO return generic og image
+			ctx.body = globalpingOG;
+			ctx.type = 'image/png';
+			ctx.maxAge = 60;
+			return;
 		}
 
 		let svg;
@@ -707,13 +289,16 @@ module.exports.globalping = async (ctx) => {
 			}
 
 			default: {
-				svg = await ctx.render('globalping-og/og-image-ping.svg', data);
+				ctx.body = globalpingOG;
+				ctx.type = 'image/png';
+				ctx.maxAge = 60;
+				return;
 			}
 		}
 
 		ctx.body = await render(svg);
 		ctx.type = 'image/png';
-		ctx.maxAge = 24 * 60 * 60; // 1 minute unless mes.status === finished
+		ctx.maxAge = 24 * 60 * 60;
 	} catch (error) {
 		console.log(error);
 
