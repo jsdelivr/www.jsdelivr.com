78b20b3868189275f9cc16434ac5367bbf5eaca0
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2025-04-10 14:55:47 +0330

    Move unrender listeners to util file

--- src/assets/js/utils/listeners.js
+++ src/assets/js/utils/listeners.js
@@ -12,5 +12,26 @@ const addManagedListener = (component, target, event, handler) => {
 module.exports.addManagedListener = addManagedListener;
 
 module.exports.screenWidgetListener = (component) => {
-	addManagedListener(component, window, 'resize', debounce(throttle(() => this.set('screenWidth', window.innerWidth), 200)));
+	addManagedListener(component, window, 'resize', debounce(throttle(() => component.set('screenWidth', window.innerWidth), 200)));
+};
+
+module.exports.stickySidebarScrollListener = (component) => {
+	let sidebar = document.querySelector('.page-content_side-menu');
+
+	if (sidebar) {
+		let top = sidebar.getBoundingClientRect().top + document.body.scrollTop;
+
+		let handleStickySidebarScroll = () => {
+			let y = document.scrollingElement.scrollTop;
+			let currentSidebarTop = sidebar.getBoundingClientRect().top;
+
+			if ((currentSidebarTop <= 20 && currentSidebarTop >= 0) || y > top) {
+				sidebar.setAttribute('style', 'position: sticky; top: 20px');
+			} else {
+				sidebar.removeAttribute('style');
+			}
+		};
+
+		addManagedListener(component, window, 'scroll', handleStickySidebarScroll);
+	}
 };
