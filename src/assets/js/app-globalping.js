7c8b730a15786b566bb11a3098241e08de85c22c
xbpcb <ch.kyrill@gmail.com>
2025-05-19 19:40:15 +0300

    App: add temp route for GpISP

--- src/assets/js/app-globalping.js
+++ src/assets/js/app-globalping.js
@@ -13,6 +13,7 @@ const cGlobalpingSponsors = require('../../views/pages/globalping/sponsors.html'
 const cGlobalpingCredits = require('../../views/pages/globalping/credits.html');
 const cGlobalpingNetwork = require('../../views/pages/globalping/network.html');
 const cGlobalpingUsers = require('../../views/pages/globalping/_users.html');
+const cGlobalpingISP = require('../../views/pages/globalping/isp.html');
 const cPP = require('../../views/pages/globalping/terms.html');
 const { getGlobalpingUser } = require('./utils/http');
 
@@ -41,6 +42,7 @@ app.router.addRoute('/about-us', cGlobalpingAbout);
 app.router.addRoute('/sponsors', cGlobalpingSponsors);
 app.router.addRoute('/credits', cGlobalpingCredits);
 app.router.addRoute('/network', cGlobalpingNetwork, { qs: [ 'filter' ] });
+app.router.addRoute('/isp', cGlobalpingISP);
 app.router.addRoute('/terms', cPP);
 app.router.addRoute('/terms/:currentPolicy', cPP);
 app.router.addRoute('/users/:username', cGlobalpingUsers);
