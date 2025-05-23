2065979fec6fe79d475f9c50318b12ea78387ccc
xbpcb <ch.kyrill@gmail.com>
2025-05-23 18:21:11 +0300

    AppGp: mod ISP route

--- src/assets/js/app-globalping.js
+++ src/assets/js/app-globalping.js
@@ -42,7 +42,7 @@ app.router.addRoute('/about-us', cGlobalpingAbout);
 app.router.addRoute('/sponsors', cGlobalpingSponsors);
 app.router.addRoute('/credits', cGlobalpingCredits);
 app.router.addRoute('/network', cGlobalpingNetwork, { qs: [ 'filter' ] });
-app.router.addRoute('/isp', cGlobalpingISP);
+app.router.addRoute('/isp/:ispName?', cGlobalpingISP);
 app.router.addRoute('/terms', cPP);
 app.router.addRoute('/terms/:currentPolicy', cPP);
 app.router.addRoute('/users/:username', cGlobalpingUsers);
