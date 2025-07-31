14b1a5d4a19a3f0352da54f824ad10711606eba8
Martin Kolárik <martin@kolarik.sk>
2025-07-31 15:39:03 +0200

    Move signIn and signOut methods to global app object

--- src/assets/js/app-globalping.js
+++ src/assets/js/app-globalping.js
@@ -49,11 +49,20 @@ app.router.addRoute('/terms/:currentPolicy', cPP);
 app.router.addRoute('/networks/:networkName', cGlobalpingNetworks);
 app.router.addRoute('/users/:username', cGlobalpingUsers);
 
-
 app.router.replaceQueryParam = function (name, newValue) {
 	history.replaceState(history.state, null, location.href.replace(new RegExp(`${name}=[^&]+|$`), `${name}=${encodeURIComponent(newValue)}`));
 	this.route.view.set(name, newValue);
 	return this;
+};
+
+app.signIn = () => {
+	let url = new URL('https://dash-directus.globalping.io/auth/login/github');
+	url.searchParams.set('redirect', `${Ractive.sharedGet('serverHost')}/auth/callback?redirect=${encodeURIComponent(location.href)}`);
+	location.href = url.toString();
+};
+
+app.signOut = () => {
+	http.gpLogOut().then(() => Ractive.sharedSet('user', null));
 };
 
 _.onDocumentReady(() => {
